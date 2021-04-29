45413bdddddec3bdb1c94381b903241d09fc58bf
Tommaso Agostini <tommasoagostini90@gmail.com>
2021-04-29 19:43:59 +0200

    Milestone 2

--- js/script.js
+++ js/script.js
@@ -29,27 +29,8 @@ var app = new Vue(
                     this.movies = result
                     console.log(result);
                     this.movieName = '';
-                });
-                axios
-                .get('https://api.themoviedb.org/3/search/tv', {
-                    params: {
-                        "api_key": '6c5e72b5c6b0f7602a85d01cf04bc5cd',
-                        "query": this.movieName
-                    }
-                })  
-                .then((response) => {
-                    let result = response.data.results;
-                    this.movies = result
-                    console.log(result);
-                    this.movieName = '';
-                });
-
-
-                
+                }); 
             }   
-        },
-        mounted() {
-            
         }
     }
 )
